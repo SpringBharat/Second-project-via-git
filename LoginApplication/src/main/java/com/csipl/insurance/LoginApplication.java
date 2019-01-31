c5bbed0f99da2f8a2d36e06282215a92ed3740c4
bharat <bharat.patel@computronics.in>
2019-01-31 17:20:18 +0530

    Hello

--- LoginApplication/src/main/java/com/csipl/insurance/LoginApplication.java
+++ LoginApplication/src/main/java/com/csipl/insurance/LoginApplication.java
@@ -1,13 +1,22 @@
 package com.csipl.insurance;
 
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LoginApplication {
+public class LoginApplication  implements ApplicationRunner  {
 
 	public static void main(String[] args) {
+		
 		SpringApplication.run(LoginApplication.class, args);
+	}
+
+	@Override
+	public void run(ApplicationArguments args) throws Exception {
+	System.out.println("LoginApplication.run()");
+		
 	}
 
 }
